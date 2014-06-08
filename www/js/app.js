7ef6129abdf80db6e6bec8343875e34a22a70594
vipin <vipinsharma_03@yahoo.com>
2014-06-08 13:38:24 +0530

    css updated

--- www/js/app.js
+++ www/js/app.js
@@ -8,13 +8,23 @@ var sampleapp=angular.module('sampleApp', ['ngRoute'])
       when('/abc', {
           templateUrl: 'View/Home.html',
           controller: 'HomeContoller'
-      }).      
+      }).
+      when('/add', {
+          templateUrl: 'View/add_order.html',
+          controller: 'test'
+      }).    
       otherwise({
       
           redirectTo: '/abc'
       });
   } ]);
 
+
+  sampleapp.controller('test', function ($scope) {
+      $scope.go = function (path) {
+          $location.path(path);
+      };
+  })
 
   sampleapp.controller('HomeContoller', function ($scope) {
 
